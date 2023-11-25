20de309fb7debf6e6af97e306c8aeb0f8df196ca
Arya Rajput <91250229+Arya2210@users.noreply.github.com>
2023-11-25 20:18:41 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose") 
-const url = "mongodb://127.0.0.1:27017/QuestionPaper"
+const url = "mongodb+srv://arya22rajput:svmNx5ZL7zQj1plh@cluster0.htlhllh.mongodb.net/QuestionPaper"
 
 exports.dbconnect = ()=>{
     mongoose.connect( url ,{
